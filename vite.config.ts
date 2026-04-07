8c5f7e2ce7d2078f5e9a85298e3ad9ac350c0427
Emraan Mugloo <132273517+ImranSir09@users.noreply.github.com>
2026-04-07 21:19:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+base: '/PM-POSHAN-PRO-V2/',
+plugins: [react()],
+resolve: {
+alias: {
+'@': '/src',
+},
+},
 });
