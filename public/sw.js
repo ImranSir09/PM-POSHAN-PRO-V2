8d73d63ddfb0c54ff27b7bd6bcfb3d581b325956
Emraan Mugloo <132273517+ImranSir09@users.noreply.github.com>
2026-04-08 08:45:54 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,59 +1,51 @@
-// auto service worker for PM POSHAN PRO
-
 const CACHE_NAME = "pm-poshan-pro-v1";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./screenshot1.png"
+"./",
+"./index.html",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png",
+"./screenshot1.png"
 ];
 
-// install
 self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.skipWaiting();
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+);
 });
 
-// activate
 self.addEventListener("activate", event => {
-  self.clients.claim();
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
+self.clients.claim();
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.map(key => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
+})
+)
+)
+);
 });
 
-// fetch
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+if (event.request.method !== "GET") return;
 
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response;
-
-      return fetch(event.request).then(networkResponse => {
-        const clone = networkResponse.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, clone);
-        });
-        return networkResponse;
-      }).catch(() => {
-        return caches.match("./index.html");
-      });
-    })
-  );
+event.respondWith(
+caches.match(event.request).then(response => {
+return (
+response ||
+fetch(event.request).then(networkResponse => {
+const clone = networkResponse.clone();
+caches.open(CACHE_NAME).then(cache => {
+cache.put(event.request, clone);
+});
+return networkResponse;
+}).catch(() => caches.match("./index.html"))
+);
+})
+);
 });
