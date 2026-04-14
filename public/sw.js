db9bc46e44d7ba99e1d94ec83afae53131579145
Emraan Mugloo <132273517+ImranSir09@users.noreply.github.com>
2026-04-14 11:18:50 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,18 @@
-const CACHE_NAME = "pm-poshan-pro-v23";
+const CACHE_NAME = "pm-poshan-pro-v24";
 
 const urlsToCache = [
-"./",
-"./index.html",
-"./index.tsx",
-"./manifest.json",
-"./icon-192.png",
-"./icon-512.png",
-"./maskable-192.png",
-"./maskable-512.png",
+"../",
+"../index.html",
+"../index.tsx",
+"../manifest.json",
+"../icon-192.png",
+"../icon-512.png",
+"../maskable-192.png",
+"../maskable-512.png",
 
 "https://cdn.tailwindcss.com",
 "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
-"https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.23/jspdf.plugin.autotable.min.js",
-
-"https://aistudiocdn.com/react@^19.1.1",
-"https://aistudiocdn.com/react-dom@^19.1.1",
-"https://aistudiocdn.com/recharts@^2.12.7"
+"https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.23/jspdf.plugin.autotable.min.js"
 ];
 
 self.addEventListener("install", event => {
@@ -44,7 +40,7 @@ response || fetch(event.request).then(networkResponse => {
 const clone = networkResponse.clone();
 caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
 return networkResponse;
-}).catch(() => caches.match("./index.html"))
+}).catch(() => caches.match("../index.html"))
 )
 );
 });
