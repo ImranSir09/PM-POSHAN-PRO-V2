a4d5f5694d4996db935b385b18d45aa25861b824
Emraan Mugloo <132273517+ImranSir09@users.noreply.github.com>
2026-04-14 22:15:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-base: '/PM-POSHAN-PRO-V2/',
+base: './',
 plugins: [react()],
 resolve: {
 alias: {
