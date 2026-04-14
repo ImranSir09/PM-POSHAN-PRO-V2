74b63a63e727c472a1949143989de73b5979e484
Emraan Mugloo <132273517+ImranSir09@users.noreply.github.com>
2026-04-14 20:22:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
-import { defineConfig } from 'vite';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+export default defineConfig(({ mode }) => {
+const env = loadEnv(mode, '.', '');
+
+return {
+base: '/PM-POSHAN-PRO-V2/',
+
+plugins: [react()],
+
+build: {
+  outDir: 'dist'
+},
+
+define: {
+  'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+  'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+},
+
+resolve: {
+  alias: {
+    '@': path.resolve(__dirname, '.'),
+  }
+}
+
+};
 });
