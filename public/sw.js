f3031e3ab6f434c9f46eee68aec5179e05c0b86d
Emraan Mugloo <132273517+ImranSir09@users.noreply.github.com>
2026-04-14 09:05:32 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pm-poshan-pro-v17";
+const CACHE_NAME = "pm-poshan-pro-v18";
 
 const urlsToCache = [
 "./",
@@ -36,11 +36,16 @@ self.addEventListener("fetch", event => {
 
 if (event.request.method !== "GET") return;
 
-if (event.request.url.includes("manifest.json")) return;
-
 if (event.request.mode === "navigate") {
 event.respondWith(
 fetch(event.request)
+.then(response => {
+const clone = response.clone();
+caches.open(CACHE_NAME).then(cache => {
+cache.put(event.request, clone);
+});
+return response;
+})
 .catch(() => caches.match("./index.html"))
 );
 return;
@@ -48,16 +53,13 @@ return;
 
 event.respondWith(
 caches.match(event.request).then(response => {
-return (
-response ||
-fetch(event.request).then(networkResponse => {
+return response || fetch(event.request).then(networkResponse => {
 const clone = networkResponse.clone();
 caches.open(CACHE_NAME).then(cache => {
 cache.put(event.request, clone);
 });
 return networkResponse;
-})
-);
+});
 })
 );
 
