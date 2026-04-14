79a034fcbdf859dc8e27409c75771eded1269e70
Emraan Mugloo <132273517+ImranSir09@users.noreply.github.com>
2026-04-14 11:48:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,15 @@
-const CACHE_NAME = "pm-poshan-pro-v26";
+const CACHE_NAME = "pm-poshan-pro-safe";
 
-const urlsToCache = [
-"../",
-"../index.html",
-"../manifest.json",
-"../icon-192.png",
-"../icon-512.png",
-"../maskable-192.png",
-"../maskable-512.png"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener("install", e => {
 self.skipWaiting();
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-);
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", e => {
 self.clients.claim();
-event.waitUntil(
-caches.keys().then(keys =>
-Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
-)
-);
 });
 
-self.addEventListener("fetch", event => {
-
-if (event.request.method !== "GET") return;
-
-if (event.request.mode === "navigate") {
-event.respondWith(
-fetch(event.request).catch(() => caches.match("../index.html"))
-);
-return;
+self.addEventListener("fetch", e => {
+if (e.request.mode === "navigate") {
+e.respondWith(fetch(e.request));
 }
-
-event.respondWith(
-fetch(event.request)
-.then(response => {
-const clone = response.clone();
-caches.open(CACHE_NAME).then(cache => {
-cache.put(event.request, clone);
-});
-return response;
-})
-.catch(() => caches.match(event.request))
-);
-
 });
