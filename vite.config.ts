40bb5c14df4444928d245b7200b72a43b945a9fe
Emraan Mugloo <132273517+ImranSir09@users.noreply.github.com>
2026-04-15 12:35:51 +0530

    feat: Update build and deployment configuration
    Refactor the Vite build configuration and update the GitHub Actions workflow for deployment. This includes adjusting the base path for the build output and ensuring CDN imports are correctly handled for improved loading performance and offline capabilities.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-base: './',
-plugins: [react()],
-resolve: {
-alias: {
-'@': path.resolve(__dirname, '.'),
-}
-}
-})
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
